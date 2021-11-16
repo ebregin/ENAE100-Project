b91d6111cb66d17ab99d79c42008bb496c16c23f
ebregin <71725639+ebregin@users.noreply.github.com>
2021-11-16 16:59:29 -0500

    Update Rocket_sim.m

--- Rocket_sim.m
+++ Rocket_sim.m
@@ -1,8 +1,10 @@
 % ENAE100
 % Simulation for rocket 
+clear
+clc
+close all
 
-
-global thrust_time T0 mf m0 A Cd;
+global thrust_time T0 mf m0 A Cd_rocket Cd_parachute Parachute_area;
 
 mass_rocket = .693; %Kg
 m0=mass_rocket+.202; %Kg
@@ -10,16 +12,39 @@ mf=mass_rocket+.09; %Kg
 T0=165; %N
 thrust_time=1; %burn time
 A=10.36358/1550; %m^2
-Cd=.5;
+Cd_rocket=.6;
+Cd_parachute = .75;
+Parachute_area= pi*0.6096^2/4;
 x0=[0 0];
-tspan=[0 40];
+tspan=[0 100];
+tme = 10; %s motor ejection delay
 
-[tout,xout] = ode45(@xdotsimplerocket,tspan,x0);
+[tout,xout] = ode45(@xdotsimplerocket,tspan,x0,[],tme);
+
+tout=tout(xout(:,1)>0);
+temp1=xout(xout(:,1)>0,1);
+temp2=xout(xout(:,1)>0,2);
+xout=[temp1 , temp2];
+
+figure
+hold on
+subplot(2,1,1)
+plot(tout,xout(:,1))
+ylabel('Altitude (m)')
+xlabel('Time (s)')
+title('Model Rocket Sim')
+grid on
+subplot(2,1,2)
+plot(tout,xout(:,2))
+ylabel('Velocity (m/s)')
+xlabel('Time (s)')
+grid on
 
 
 
-function xdot = xdotsimplerocket(t,x)
-global thrust_time T0 mf m0 A Cd;
+
+function xdot = xdotsimplerocket(t,x,tme)
+global thrust_time T0 mf m0 A  Cd_rocket Cd_parachute Parachute_area;
 
 if t<thrust_time
     T=T0;
@@ -31,9 +56,15 @@ else
     m_dot=0;
 end
     
+if t>tme
+    Cd_p=Cd_parachute;
+else
+    Cd_p=0;
+end
+
 xdot(1,1) =  x(2);
 
-xdot(2,1) = (T-sign(x(2))*.5*rho(x(1))*x(2)^2*Cd*A-9.8*m-2*m_dot*x(2))/m;
+xdot(2,1) = (T-sign(x(2))*.5*rho(x(1))*x(2)^2*(Cd_rocket*A+Cd_p*Parachute_area)-9.8*m-2*m_dot*x(2))/m;
 
 end
 
